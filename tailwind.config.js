fbad53785599103f8dc9ec8bc90fe055af1191d7
shuvo-me <shuvo.js.dev@gmail.com>
2024-04-24 18:34:45 +0600

    update: tailwind config for container

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,20 @@ export default {
     "./error.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+     
+    },
+    container:{
+     center: true,
+     padding: '1rem',
+     screens: {
+       sm: '600px',
+       md: '728px',
+       lg: '984px',
+       xl: '1240px',
+       '2xl': '1496px',
+     },
+    }
   },
   plugins: [],
 }
