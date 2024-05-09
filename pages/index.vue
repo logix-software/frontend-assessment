5781a7ee2e63802fe8578496c8c426497c5b4c5c
shuvo-me <shuvo.js.dev@gmail.com>
2024-05-09 15:15:31 +0600

    fix: responsive hero text

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-    <section class="mt-[174px]">
+    <section class="mt-10 lg:mt-[174px]">
         <div class="container">
             <div>
-                <h1 class=" text-[86px] text-white text-center !leading-[96px]">
+                <h1 class=" text-3xl md:text-5xl lg:text-[86px] text-white text-center  lg:!leading-[96px]">
                     PORTA IL TUO
                     <span class="italic font-medium relative">
                         PADEL CLUB
                         <span
-                            class=" bg-secondary text-app-bg text-2xl px-4 py-2.5 -top-8 -right-12 absolute transform rotate-[13.9deg]">A
+                            class=" bg-secondary text-app-bg text-2xl px-4 py-2.5 -top-8 -right-12 hidden md:inline absolute transform rotate-[13.9deg]  my-2 lg:my-0">A
                             COSTO
                             ZERO</span>
                     </span> <br>
@@ -16,7 +16,7 @@
                         LIVELLO SUCCESSIVO
                     </span>
                 </h1>
-                <p class="text-white text-center text-3xl mt-6">
+                <p class="text-white text-center text-lg md:text-xl lg:text-3xl mt-6">
                     Aumenta gli accessi, ospita gli eventi esclusivi e i nostri viaggi <br> allenamento. Ci occuperemo
                     di
                     supportarti nell’intera organizzazione.
