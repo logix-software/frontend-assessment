ccbc6ece9266681a3ac6005f9f12f56d44893c06
shuvo-me <shuvo.js.dev@gmail.com>
2024-05-09 16:07:19 +0600

    design: project section

--- components/ProjectSection.vue
+++ components/ProjectSection.vue
@@ -1,13 +1,30 @@
 <template>
-    <section class="mt-[188px]">
-        <div class="container">
-            <p>
+    <section class="mt-20 lg:mt-[188px]">
+        <div class="container relative">
+            <p class="text-white text-3xl md:text-6xl">
                 Il Padel World Club DAO <br>
                 mira a
-                <span class="flex items-center gap-1">
+                <span class="inline-flex items-center gap-1 bg-secondary text-app-bg px-2 my-1">
                     rivoluzionare
+                    <icon-rotate />
+                </span> <br>
+
+                l'esperienza del
+                <span class="inline-flex items-center gap-1 bg-secondary text-app-bg px-2 my-1">
+                    padel
+                    <icon-livello />
                 </span>
+                creando <br>
+                un circuito elitario di club <br>
+                diffusi <span class="inline-flex items-center gap-1 bg-secondary text-app-bg  px-2 my-1">
+                    globalmente
+                    <icon-globe />
+                </span> attraverso <br>
+                la tecnologia web3.
             </p>
+            <span
+                class=" text-app-bg bg-[#D3E2E8] px-2 md:px-4 py-2 text-xl md:text-3xl absolute  -top-20 left-6 md:-left-2 rotate-[-5.45deg]">IL
+                PROGETTO</span>
         </div>
     </section>
 </template>
---
+++ components/icon/Rotate.vue
@@ -0,0 +1,15 @@
+<template>
+    <svg width="57" height="57" viewBox="0 0 57 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_1_1213)">
+            <path
+                d="M25.6242 36.9398L34.5326 39.3268L32.1456 48.2353M45.1928 30.3484C48.52 25.5923 49.5339 20.5791 47.3773 16.8504C43.6743 10.4301 31.9991 10.2354 21.3024 16.4112C10.6056 22.587 4.93661 32.7954 8.64241 39.214C12.2738 45.5005 23.551 45.8177 34.0624 40.0219"
+                stroke="#00282E" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round" />
+        </g>
+        <defs>
+            <clipPath id="clip0_1_1213">
+                <rect width="45.7435" height="45.7435" fill="white"
+                    transform="translate(11.8393 0.0194092) rotate(15)" />
+            </clipPath>
+        </defs>
+    </svg>
+</template>
