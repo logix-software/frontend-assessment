568edc07cde3a4836ab0c08a72f0405380816c41
shuvo-me <shuvo.js.dev@gmail.com>
2024-05-09 12:08:32 +0600

    fix: hader responsive

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -4,7 +4,7 @@
             <div>
                 <img src="/app-logo.svg" alt="app-logo">
             </div>
-            <div class="py-4">
+            <div class="py-4 hidden lg:inline">
                 <ul class="flex items-center justify-center gap-7">
                     <li>
                         <nuxt-link
@@ -50,10 +50,19 @@
                     </li>
                 </ul>
             </div>
-            <div>
+            <div class="hidden lg:inline">
                 <nuxt-link to="/contact" class="btn-primary">
                     Contattaci
                 </nuxt-link>
+            </div>
+            <div class="lg:hidden">
+                <button class=" size-12 rounded inline-flex items-center justify-center bg-primary text-white">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                        <path fill-rule="evenodd"
+                            d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </button>
             </div>
         </div>
 
