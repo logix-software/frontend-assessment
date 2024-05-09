8b5e815752ef39cb95197f7925d21dc892a3e85f
shuvo-me <shuvo.js.dev@gmail.com>
2024-05-09 15:06:43 +0600

    design: hero text section

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,28 @@
 <template>
-    home page
+    <section class="mt-[174px]">
+        <div class="container">
+            <div>
+                <h1 class=" text-[86px] text-white text-center !leading-[96px]">
+                    PORTA IL TUO
+                    <span class="italic font-medium relative">
+                        PADEL CLUB
+                        <span
+                            class=" bg-secondary text-app-bg text-2xl px-4 py-2.5 -top-8 -right-12 absolute transform rotate-[13.9deg]">A
+                            COSTO
+                            ZERO</span>
+                    </span> <br>
+                    AL
+                    <span class=" italic font-medium">
+                        LIVELLO SUCCESSIVO
+                    </span>
+                </h1>
+                <p class="text-white text-center text-3xl mt-6">
+                    Aumenta gli accessi, ospita gli eventi esclusivi e i nostri viaggi <br> allenamento. Ci occuperemo
+                    di
+                    supportarti nell’intera organizzazione.
+                </p>
+            </div>
+
+        </div>
+    </section>
 </template>
