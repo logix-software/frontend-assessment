24c14083cf2d5c7b5551cd61011bf68536bcf576
shuvo-me <shuvo.js.dev@gmail.com>
2024-05-09 12:23:09 +0600

    design: footer section

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,10 +1,35 @@
 <template>
-    <footer class="mt-auto">
-        <div class="container">
-            <div>
+    <footer class="mt-auto py-8">
+        <div class="container flex items-end justify-between flex-wrap gap-6">
+            <div class="space-y-4">
                 <div>
                     <img src="/app-logo.svg" alt="app logo footer">
                 </div>
+                <div>
+                    <ul class="flex items-center gap-3">
+                        <li>
+                            <nuxt-link to="/"
+                                class="text-xs font-bold text-white hover:text-primary transition duration-200 ease-linear">
+                                Contact
+                            </nuxt-link>
+                        </li>
+                        <li>
+                            <nuxt-link to="/"
+                                class="text-xs font-bold text-white hover:text-primary transition duration-200 ease-linear">
+                                Privacy policy
+                            </nuxt-link>
+                        </li>
+                        <li>
+                            <nuxt-link to="/"
+                                class="text-xs font-bold text-white hover:text-primary transition duration-200 ease-linear">
+                                Terms
+                            </nuxt-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <small class="text-xs text-white">Copyright © 2024 PadelDao. All rights reserved</small>
             </div>
         </div>
     </footer>
