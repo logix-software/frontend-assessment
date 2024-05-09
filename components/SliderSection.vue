77bce5b9716df532ec94b2c7ec4648d4b1c1c336
shuvo-me <shuvo.js.dev@gmail.com>
2024-05-09 20:58:19 +0600

    fix: slide issues & custom prev,next button

--- components/SliderSection.vue
+++ components/SliderSection.vue
@@ -1,7 +1,45 @@
 <script setup>
 import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/navigation';
 
+const swiperRef = ref(null)
+const prevBtnRef = ref(null)
+const nextBtnRef = ref(null)
+const modules = [Navigation]
+
+const prev = () => {
+    swiperRef?.value?.slidePrev()
+}
+
+const next = () => {
+    swiperRef.value.slideNext()
+}
+
+const initiateSwiperInstance = (swiperInstance) => {
+    console.log({ swiperInstance })
+    swiperRef.value = swiperInstance
+}
+
+const handleSlideChange = (swp) => {
+    console.log({ swp })
+}
+
+const breakPoints = {
+    '300': {
+        slidesPerView: 1
+    },
+    '550': {
+        slidesPerView: 2
+    },
+    '750': {
+        slidesPerView: 3,
+    },
+    '950': {
+        slidesPerView: 4,
+    }
+}
 </script>
 
 <template>
@@ -14,19 +52,20 @@ import 'swiper/css';
                         Già <b>9 padel club</b> hanno scelto di entrare <br> nella community PadelDAO.
                     </p>
                 </div>
-                <div class="flex items-center gap-3">
-                    <button
+                <div class="flex items-center gap-3 ml-auto">
+                    <button ref="prevBtnRef" @click="prev"
                         class="size-16 inline-flex items-center justify-center text-white hover:bg-primary transition duration-200 ease-linear bg-[#001214]">
                         <icon-arrow-left />
                     </button>
-                    <button
+                    <button ref="nextBtnRef" @click="next"
                         class="size-16 inline-flex items-center justify-center text-white hover:bg-primary transition duration-200 ease-linear bg-[#001214]">
                         <icon-arrow-right />
                     </button>
                 </div>
             </div>
             <div class="mt-[71px]">
-                <swiper :slidesPerView="4" :spaceBetween="18" class="mySwiper">
+                <swiper :loop="true" :breakpoints="breakPoints" :slidesPerView="4" :modules="modules" :spaceBetween="18"
+                    class="mySwiper" ref="swiperRef" @swiper="initiateSwiperInstance">
                     <swiper-slide v-for="n in 10" :key="n">
                         <img src="/slider-img.png" alt="slider img" class="w-full" />
                     </swiper-slide>
