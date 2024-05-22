52ed0ef3bbc9cad9fa676565c61a5df95d546192
shuvo-me <shuvo.js.dev@gmail.com>
2024-05-22 14:35:57 +0600

    refactor: templating in form-input component.

--- components/FormInput.vue
+++ components/FormInput.vue
@@ -1,5 +1,15 @@
+<template>
+    <div class="flex flex-col gap-3">
+        <label v-text="inputLabel" :for="id" class=" text-white text-lg font-medium" />
+        <input :id="id" :type="inputType" :placeholder="inputPlaceholder"
+            class=" bg-white py-5 px-7 outline-0 focus:ring-1 focus:ring-primary placeholder:text-lg placeholder:text-[#809396] placeholder:font-medium text-lg font-medium"
+            v-model="value" />
+    </div>
+</template>
+
 <script setup>
 
+// props
 defineProps({
     id: {
         type: String,
@@ -15,14 +25,7 @@ defineProps({
     }
 })
 
+// data-binding
 const value = defineModel()
-</script>
 
-<template>
-    <div class="flex flex-col gap-3">
-        <label v-text="inputLabel" :for="id" class=" text-white text-lg font-medium" />
-        <input :id="id" :type="inputType" :placeholder="inputPlaceholder"
-            class=" bg-white py-5 px-7 outline-0 focus:ring-1 focus:ring-primary placeholder:text-lg placeholder:text-[#809396] placeholder:font-medium text-lg font-medium"
-            v-model="value" />
-    </div>
-</template>
+</script>
